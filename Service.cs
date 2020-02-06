410b4cb84f0f90c8a1962658a18eeca0589dbf55
Soledad Unda <51492868+ASoledadU@users.noreply.github.com>
2020-02-06 12:07:23 -0600

    Setting up a service
    This is defining what a service is and will help when we want to call on it later.

---
+++ Service.cs
@@ -0,0 +1,12 @@
+namespace VSC
+{
+    public class Service
+    {
+        public string Name {get; set; }
+        public decimal Price {get; set;}
+        public Service(string name, decimal price) {
+            Name = name;
+            Price = price;
+        }
+    }
+}
